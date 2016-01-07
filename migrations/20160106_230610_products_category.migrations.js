cc1cb4c1a7e58cc73f2411dbaf827962bab3ad5b
Marcos Bérgamo <markusdarkness@gmail.com>
2016-01-06 23:34:30 -0200

    Add migration for products and category

---
+++ migrations/20160106_230610_products_category.migrations.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const Promise = require('bluebird');
+
+module.exports = { up, down };
+
+function up (db) {
+  const Product = require('../src/catalog/product/model')(db.sequelize, db.Sequelize);
+  const Category = require('../src/catalog/category/model')(db.sequelize, db.Sequelize);
+  Product.associate(db);
+  Category.associate(db);
+  return Promise.all([Product.sync(), Category.sync()]);
+}
+
+function down (db) {
+  const Product = require('../src/catalog/product/model')(db.sequelize, db.Sequelize);
+  const Category = require('../src/catalog/category/model')(db.sequelize, db.Sequelize);
+  return Promise.all([Product.sync(), Category.sync()]);
+}
+
