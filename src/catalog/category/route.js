89bcbd5b9d9d2d2b924235fb7013526ea3e6c8ef
Marcos Bérgamo <markusdarkness@gmail.com>
2016-01-07 00:07:36 -0200

    Add category route

---
+++ src/catalog/category/route.js
@@ -0,0 +1,62 @@
+'use strict';
+
+const Controller = require('./controller');
+const Validator = require('./validation');
+
+exports.register = (server, options, next) => {
+  // instantiate controller
+  const controller = new Controller(options.database);
+  
+  server.bind(controller);
+  server.route([
+    {
+      method: 'GET',
+      path: '/category',
+      config: {
+        handler: controller.list,
+        validate: Validator.list()
+      }
+    },
+    {
+      method: 'GET',
+      path: '/category/{id}',
+      config: {
+        handler: controller.read,
+        validate: Validator.read()
+      }
+    },
+    {
+      method: 'POST',
+      path: '/category',
+      config: {
+        handler: controller.create,
+        validate: Validator.create()
+      }
+    },
+    {
+      method: 'PUT',
+      path: '/category/{id?}',
+      config: {
+        handler: controller.update,
+        validate: Validator.update()
+      }
+    },
+    {
+      method: 'DELETE',
+      path: '/category/{id?}',
+      config: {
+        handler: controller.destroy,
+        validate: Validator.destroy()
+      }
+    }
+  ]);
+
+  next();
+};
+
+exports.register.attributes = {
+  name: 'category',
+  version: '1.0.0'
+};
+
+
