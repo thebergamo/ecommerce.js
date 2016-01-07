d173011f33410ac7f14e57249d645a7dec022e6a
Marcos Bérgamo <markusdarkness@gmail.com>
2016-01-06 23:34:02 -0200

    Add product model

---
+++ src/catalog/product/model.js
@@ -0,0 +1,47 @@
+'use strict';
+
+module.exports = (sequelize, DataType) => {
+  let Product = sequelize.define('Product', {
+    id: {
+      type: DataType.INTEGER,
+      autoincrement: true,
+      primaryKey: true
+    },
+    name: {
+      type: DataType.STRING(100),
+      allowNull: false
+    },
+    description: {
+      type: DataType.TEXT,
+      allowNull: false,
+      defaultValue: ''
+    },
+    model: {
+      type: DataType.STRING(50),
+      allowNull: true
+    },
+    upc: {
+      type: DataType.STRING(13),
+      allowNull: true
+    },
+    price: {
+      type: DataType.DECIMAL(14, 2),
+      allowNull: false,
+      defaultValue: 0.00
+    },
+    status: {
+      type: DataType.BOOLEAN,
+      allowNull: false,
+      defaultValue: false
+    }
+  }, {
+    classMethods: {
+      associate: (models) => {
+        Product.belongsToMany(models.Category,
+          { through: 'ProductCategory'});
+      }
+    }
+  });
+
+  return Product;
+}
