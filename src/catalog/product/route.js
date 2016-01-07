5c3c03799912dedcdf388aa45aeca5caad6a300f
Marcos Bérgamo <markusdarkness@gmail.com>
2016-01-06 23:44:42 -0200

    Add product route

---
+++ src/catalog/product/route.js
@@ -0,0 +1,61 @@
+'use strict';
+
+const Controller = require('./controller');
+const Validator = require('./validation');
+
+exports.register = (server, options, next) => {
+  // instantiate controller
+  const controller = new Controller(options.database);
+  
+  server.bind(controller);
+  server.route([
+    {
+      method: 'GET',
+      path: '/product',
+      config: {
+        handler: controller.list,
+        validate: Validator.list()
+      }
+    },
+    {
+      method: 'GET',
+      path: '/product/{id}',
+      config: {
+        handler: controller.read,
+        validate: Validator.read()
+      }
+    },
+    {
+      method: 'POST',
+      path: '/product',
+      config: {
+        handler: controller.create,
+        validate: Validator.create()
+      }
+    },
+    {
+      method: 'PUT',
+      path: '/product/{id?}',
+      config: {
+        handler: controller.update,
+        validate: Validator.update()
+      }
+    },
+    {
+      method: 'DELETE',
+      path: '/product/{id?}',
+      config: {
+        handler: controller.destroy,
+        validate: Validator.destroy()
+      }
+    }
+  ]);
+
+  next();
+};
+
+exports.register.attributes = {
+  name: 'product',
+  version: '1.0.0'
+};
+
