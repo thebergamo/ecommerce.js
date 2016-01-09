9938e68a835607ce6895d08c8c23bb39c378ede3
Marcos Bérgamo <markusdarkness@gmail.com>
2016-01-09 01:14:54 -0200

    Fix migration product_category

--- migrations/20160106_230610_products_category.migrations.js
+++ migrations/20160106_230610_products_category.migrations.js
@@ -7,14 +7,20 @@ module.exports = { up, down };
 function up (db) {
   const Product = require('../src/catalog/product/model')(db.sequelize, db.Sequelize);
   const Category = require('../src/catalog/category/model')(db.sequelize, db.Sequelize);
-  Product.associate(db);
-  Category.associate(db);
-  return Promise.all([Product.sync(), Category.sync()]);
+  const productCategory = require('../src/catalog/productCategory/model')(db.sequelize, db.Sequelize);
+  return Promise.all([Product.sync(), Category.sync()])
+  .then(() => {
+    return productCategory.sync();
+  });
 }
 
 function down (db) {
   const Product = require('../src/catalog/product/model')(db.sequelize, db.Sequelize);
   const Category = require('../src/catalog/category/model')(db.sequelize, db.Sequelize);
-  return Promise.all([Product.drop(), Category.drop()]);
+  const productCategory = require('../src/catalog/productCategory/model')(db.sequelize, db.Sequelize);
+  return Promise.all([Product.drop(), Category.drop()])
+  .then(() => {
+    return productCategory.drop();
+  });
 }
 
