008cfefebc4e2897e53f0f409f660c895d60e528
Marcos Bérgamo <markusdarkness@gmail.com>
2016-01-14 10:53:41 -0200

    Upate core server for new bootstrap

--- src/core/server.js
+++ src/core/server.js
@@ -3,17 +3,11 @@
 require('dotenv').load();
 
 // Load deps
-const _ = require('lodash');
-const fs = require('fs');
-const path = require('path');
 const Hapi = require('hapi');
-const boomDecorators = require('hapi-boom-decorators');
 
-// Load database
-let server;
 const db = require('./database');
-const log = require('./log');
-const auth = require('./auth');
+
+let server;
 
 module.exports = server = new Hapi.Server();
 
@@ -26,44 +20,4 @@ server.connection({
 // Expose database
 if (process.env.NODE_ENV === 'test') {
   server.database = db;
-}
-
-// Load routes
-let routes = ['shared', 'admin', 'catalog'].map((type) => {
-  return readDir(type);
-});
-
-let plugins = _.remove(_.flatten(routes), (pl) => { return pl !== undefined; });
-
-plugins.push({register: auth});
-plugins.push({register: boomDecorators});
-plugins.push({register: log});
-
-server.register(plugins, (err) => {
-  if (err) {
-    throw err;
-  }
-
-  return;
-});
-
-function readDir (type) {
-  return fs.readdirSync(path.join('src', type))
-    .filter((dir) => {
-      return dir.match(/^[^.]/);
-    })
-    .map((entity) => {
-      let root = path.join(__dirname, '..', type, entity, 'route.js');
-
-      try {
-        fs.statSync(root).isFile();
-      } catch (err) {
-        return;
-      }
-
-      return {
-        register: require(root),
-        options: {database: db}
-      };
-    });
 }
